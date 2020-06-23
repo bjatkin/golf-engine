471ae9da26caa805e88cce6e95eed7af10fe127b
Brandon Atkinson <bj.atkinson92@gmail.com>
2020-06-22 21:45:39 -0600

    add more files, fix fantasy_font and a new demo

---
+++ golf_server/golf_test/golf/golfSprite.go
@@ -0,0 +1,131 @@
+package golf
+
+import "math"
+
+// LoadSprs loads the sprite sheet into memory
+func (e *Engine) LoadSprs(sheet [0x3000]byte) {
+	base := spriteColBase
+	for i, b := range sheet {
+		e.RAM[i+base] = b
+	}
+}
+
+func (e *Engine) setActiveSpriteBuff(colAddr, palAddr int) {
+	c, p := toBytes(colAddr, 2, false), toBytes(palAddr, 2, false)
+	e.RAM[activeSpriteColBuff] = c[0]
+	e.RAM[activeSpriteColBuff+1] = c[1]
+	e.RAM[activeSpritePalBuff] = p[0]
+	e.RAM[activeSpritePalBuff+1] = p[1]
+}
+
+// SprOpts additional options for drawing sprites
+type SprOpts struct {
+	FlipH, FlipV   bool
+	Transparent    Col
+	PalFrom        []Col
+	PalTo          []Col
+	Width, Height  int
+	ScaleW, ScaleH float64
+	Fixed          bool
+}
+
+// Spr draws 8x8 sprite n from the sprite sheet to the
+// screen at x, y.
+func (e *Engine) Spr(n, x, y int, opts ...SprOpts) {
+	sx := n % 32
+	sy := n / 32
+	opt := SprOpts{}
+	if len(opts) > 0 {
+		opt = opts[0]
+	}
+	w := 1
+	h := 1
+	if opt.Height > 1 {
+		h = opt.Height
+	}
+	if opt.Width > 1 {
+		w = opt.Width
+	}
+	e.SSpr(sx*8, sy*8, w*8, h*8, x, y, opt)
+}
+
+// SSpr draw a rect from the sprite sheet to the screen
+// sx, sy, sw, and sh define the rect on the sprite sheet
+// dx, dy is the location to draw on the screen
+func (e *Engine) SSpr(sx, sy, sw, sh, dx, dy int, opts ...SprOpts) {
+	opt := SprOpts{}
+	if len(opts) > 0 {
+		opt = opts[0]
+	}
+	if !opt.Fixed {
+		dx -= toInt(e.RAM[cameraX:cameraX+2], true)
+		dy -= toInt(e.RAM[cameraY:cameraY+2], true)
+	}
+	if opt.ScaleH == 0 {
+		opt.ScaleH = 1
+	}
+	if opt.ScaleW == 0 {
+		opt.ScaleW = 1
+	}
+
+	cBase := toInt(e.RAM[activeSpriteColBuff:activeSpriteColBuff+2], false)
+	pBase := toInt(e.RAM[activeSpritePalBuff:activeSpritePalBuff+2], false)
+
+	for x := 0; x < sw; x++ {
+		for y := 0; y < sh; y++ {
+			pxl := e.pget(sx+x, sy+y, cBase, pBase)
+			if pxl != opt.Transparent {
+				pxl = subPixels(opt.PalFrom, opt.PalTo, pxl)
+				fx := 0
+				if opt.FlipH {
+					fx = int(float64(sw) * opt.ScaleW)
+				}
+				fy := 0
+				if opt.FlipV {
+					fy = int(float64(sh) * opt.ScaleH)
+				}
+				for scaleX := int(float64(x) * opt.ScaleW); scaleX < int(float64(x+1)*opt.ScaleW); scaleX++ {
+					for scaleY := int(float64(y) * opt.ScaleH); scaleY < int(float64(y+1)*opt.ScaleH); scaleY++ {
+						e.Pset(dx+int(math.Abs(float64(fx-scaleX))), dy+int(math.Abs(float64(fy-scaleY))), pxl)
+					}
+				}
+			}
+		}
+	}
+}
+
+// subPixels is used to swap pixels based on a pallet swap
+func subPixels(palFrom, palTo []Col, col Col) Col {
+	if len(palFrom) == 0 {
+		return col
+	}
+	for i, p := range palFrom {
+		if p == col {
+			return palTo[i]
+		}
+	}
+	return col
+}
+
+// Fget gets the fth flag on the nth sprite
+func (e *Engine) Fget(n, f int) bool {
+	return e.RAM[spriteFlags+n]&(0b00000001<<f) > 0
+}
+
+// FgetByte gets the byte flag on the nth sprite
+func (e *Engine) FgetByte(n int) byte {
+	return e.RAM[spriteFlags+n]
+}
+
+// Fset sets the fth flag on the nth sprite to s
+func (e *Engine) Fset(n, f int, s bool) {
+	e.RAM[spriteFlags+n] &= (0b00000001<<f ^ 0b11111111)
+	if s {
+		e.RAM[spriteFlags+n] |= (0b00000001 << f)
+	}
+}
+
+// FsetByte sets the byte flag on the nth sprite
+func (e *Engine) FsetByte(n int, b byte) {
+	e.RAM[spriteFlags+n] = b
+}
--- golf_server/golf_test/main.go
+++ golf_server/golf_test/main.go
@@ -3,24 +3,33 @@ package main
 import (
 	"fantasyConsole/golf_server/golf_test/golf"
 	"fmt"
+	"math"
 	"time"
 )
 
 var g *golf.Engine
 
 func main() {
-	lastFrameTime = time.Now().UnixNano()
-	g = golf.NewEngine(update, draw)
-	g.BG(golf.Col3)
-	g.DrawMouse(1)
-	g.LoadSprs(spriteSheet)
-	g.PalA(5)
-	g.PalB(4)
+	g = golf.NewEngine(demo2Update, demo2Draw)
+	g.PalA(0)
+	g.PalB(3)
 	g.Run()
+
+	// lastFrameTime = time.Now().UnixNano()
+	// g = golf.NewEngine(update, draw)
+	// g.LoadSprs(spriteSheet)
+
+	// g.BG(golf.Col3)
+	// g.PalA(0)
+	// g.PalB(3)
+
+	// g.DrawMouse(1)
+
+	// g.Run()
 }
 
 var cx, cy int
-var clipped, palDiff bool
+var clipped bool
 
 func update() {
 	if g.Btn(golf.WKey) {
@@ -34,16 +43,6 @@ func update() {
 	}
 	if g.Btn(golf.DKey) {
 		cx += 10
-	}
-	if g.Btnp(golf.PKey) {
-		if palDiff {
-			g.PalA(1)
-			g.PalB(0)
-		} else {
-			g.PalA(5)
-			g.PalB(4)
-		}
-		palDiff = !palDiff
 	}
 	if g.Btnp(golf.Space) {
 		if clipped {
@@ -73,8 +72,41 @@ func draw() {
 	g.Camera(cx, cy)
 	g.Rect(1, 10, 190, 181, golf.Col0)
 	g.TextR(fmt.Sprintf("FPS: %d", frameDiff), golf.TextOpts{Col: golf.Col0, Fixed: true})
-	g.RectFill(64, 90, 64, 5, golf.Col2)
+
+	// Shadow
+	g.RectFill(65, 90, 62, 5, golf.Col2)
+	g.RectFill(64, 91, 64, 3, golf.Col2)
 	g.Line(74, 97, 118, 97, golf.Col2)
 	g.Line(90, 99, 102, 99, golf.Col2)
-	g.SSpr(152, 0, 64, 24, 64, 64)
+
+	// Change to the internal sprite sheet
+	g.RAM[0x6F49], g.RAM[0x6F4A] = 0x36, 0x47
+	g.RAM[0x6F4B], g.RAM[0x6F4C] = 0x3C, 0x47
+
+	// Draw the logo
+	g.SSpr(152, 0, 64, 24, 64, 64, golf.SprOpts{Transparent: golf.Col2})
+
+	// Draw a scaled sprite
+	scale := (math.Sin(float64(g.Frames()) / 60)) + 1
+	g.Spr(82, 4, 13, golf.SprOpts{
+		ScaleW: scale * 2, ScaleH: scale * 2,
+		Transparent: golf.Col2,
+	})
+	g.TextL(fmt.Sprintf("Scale: %.2f", scale))
+}
+
+var bgCol = golf.Col0
+
+func demo2Update() {
+	if g.Btnp(golf.Space) {
+		bgCol++
+		if bgCol > golf.Col7 {
+			bgCol = golf.Col0
+		}
+		g.BG(bgCol)
+	}
+}
+
+func demo2Draw() {
+	g.Cls()
 }
