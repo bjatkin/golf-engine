223ee5d60841f67c12f2b3405b1d2b0ad1e41738
Brandon Atkinson <bj.atkinson92@gmail.com>
2020-08-04 23:54:58 -0600

    complie for mac, windows and linux

---
+++ util/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+GOOS=darwin GOARCH=amd64 go build -o mac/golf_toolkit *.go
+GOOS=linux GOARCH=amd64 go build -o linux/golf_toolkit *.go
+GOOS=windows GOARCH=amd64 go build -o windows/golf_toolkit *.go
